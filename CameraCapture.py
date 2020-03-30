9dea26376639f4e3850178266774920164e03426
Ubuntu <avihay@RambamAnalyticsDSMachine.wqhpxbc55couvfxpdlpoojlxmg.ax.internal.cloudapp.net>
2020-03-30 10:41:45 +0000

    backup-monitor tune

--- CameraCapture.py
+++ CameraCapture.py
@@ -142,8 +142,8 @@ class CameraCapture(object):
             AnalyzeMeasures.AnalyzeFrame(frame, self.computervision_client)
             # AnalyzeMeasures.AnalyzeFrame(frame, self.computervision_client)
         else:
-            AnalyzeFrame.AnalyzeFrame(frame, self.computervision_client, self.boundries)
-            # AnalyzeFrame2.AnalyzeFrame(frame, self.computervision_client, self.boundries)
+            # AnalyzeFrame.AnalyzeFrame(frame, self.computervision_client, self.boundries)
+            AnalyzeFrame2.AnalyzeFrame(frame, self.computervision_client, self.boundries)
 
         """
         # Endpoint URL
