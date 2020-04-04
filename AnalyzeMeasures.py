d5835888a6033b44caa35fb7c4f1062d008bffb7
Ubuntu <avihay@RambamAnalyticsDSMachine.wqhpxbc55couvfxpdlpoojlxmg.ax.internal.cloudapp.net>
2020-04-04 18:23:35 +0000

    Clean code

--- AnalyzeMeasures.py
+++ AnalyzeMeasures.py
@@ -18,6 +18,7 @@ import math
 import numpy as np
 import base64
 
+
 def create_areas(area_dict, img):
     s = img.shape
     height, width = s[0], s[1]
@@ -41,48 +42,6 @@ def transform_coords(coords, area, mode='avihay'):
     return (topleft, bottomright)
 
 
-def create_bounded_output(readings, boundings, boundries):
-    output_dict = {}
-    for key in boundries.keys():
-        for i in range(len(readings)):
-            if check_boundry(boundings[i], boundries[key]):
-                output_dict[key] = readings[i]
-        if key not in output_dict.keys():
-            output_dict[key] = "N/A"
-    return output_dict
-
-def check_boundry(bounding, boundry):
-    output = bounding[0]>=boundry[0]
-    output = output and (bounding[6]>=boundry[0])
-    output = output and (bounding[2]<=boundry[1])
-    output = output and (bounding[4]<=boundry[1])
-    output = output and (bounding[1]>=boundry[2])
-    output = output and (bounding[3]>=boundry[2])
-    output = output and (bounding[5]<=boundry[3])
-    output = output and (bounding[7]<=boundry[3])
-    return output 
-"""
-def create_bounded_output(readings, boundings, boundries, x, y):
-    output_dict = {}
-    for key in boundries.keys():
-        for i in range(len(readings)):
-            if check_boundry(boundings[i], boundries[key], x, y):
-                output_dict[key] = readings[i]
-        if key not in output_dict.keys():
-            output_dict[key] = "N/A"
-    return output_dict
-
-def check_boundry(bounding, boundry,x ,y):
-    output = bounding[0]>=boundry[0]*x
-    output = output and (bounding[6]>=boundry[0]*x)
-    output = output and (bounding[2]<=boundry[1]*x)
-    output = output and (bounding[4]<=boundry[1]*x)
-    output = output and (bounding[1]>=boundry[2]*y)
-    output = output and (bounding[3]>=boundry[2]*y)
-    output = output and (bounding[5]<=boundry[3]*y)
-    output = output and (bounding[7]<=boundry[3]*y)
-    return output  
-"""
 def fix_string(s):
     json_string_fin = ""
     last_c=""
@@ -100,6 +59,7 @@ def fix_string(s):
                 json_string_fin += "\'"
         last_c = c
     return json_string_fin
+
 
 def bounding_boxes_output_former(bbox_dict, mon_id, encoded_image):
     string_json = json.dumps(bbox_dict)
@@ -141,10 +101,7 @@ def get_digits(img, computervision_client):
                     text_flag = True
                     top_left_coords = (int(line.bounding_box[0]), int(line.bounding_box[1]))
                     bottom_right_coords = (int(line.bounding_box[4]), int(line.bounding_box[5]))
-                    # cv2.rectangle(tmp_frame, (int(line.bounding_box[0]), int(line.bounding_box[1])), (int(line.bounding_box[4]), int(line.bounding_box[5])), (255,0,0), 2)
                     cv2.rectangle(tmp_frame, top_left_coords, bottom_right_coords, (255,0,0), 2)
-                    # cv2.putText(tmp_frame,s,(int(line.bounding_box[0])-5, int(line.bounding_box[1])-5),cv2.FONT_HERSHEY_COMPLEX,0.3,(0,0,0),1)
-                    # results.append((s, line.bounding_box))
                     results.append((top_left_coords, bottom_right_coords))
                 else:
                     continue
@@ -154,30 +111,13 @@ def get_digits(img, computervision_client):
             cv2.waitKey(0)
     return(results)
 
-#TODO: incorporate along the way:
 
 def AnalyzeFrame(frame, computervision_client):
-    # COMPUTER_VISION_ENDPOINT = os.environ["COMPUTER_VISION_ENDPOINT"]
-    # COMPUTER_VISION_SUBSCRIPTION_KEY = os.environ["COMPUTER_VISION_SUBSCRIPTION_KEY"]
-    # #TODO: get client as argument
-    # computervision_client = ComputerVisionClient(COMPUTER_VISION_ENDPOINT, CognitiveServicesCredentials(COMPUTER_VISION_SUBSCRIPTION_KEY))
     frame = cv2.imdecode(np.frombuffer(frame, np.uint8), -1)
-    s = frame.shape
-    # tmp = cv2.imencode(".jpg", frame)[1]
-    # cv2.imwrite("try.jpg", tmp)
-    frame_height, frame_width = s[0], s[1]
-    # TODO: new area dict
-    areas_dict = {'side': [0, 1, 0.7, 0.9], 'bottom': [0.6, 0.9, 0.3, 0.7]}
+    # areas_dict = {'side': [0, 1, 0.7, 0.9], 'bottom': [0.6, 0.9, 0.3, 0.7]}
     areas_dict = {'low': [0.6, 0.85, 0, 0.5], 'side': [0.1, 0.9, 0.6, 0.9]}
-    # w1 = 0.7
-    # w2 = 0.9
-    # h1 = 0
-    # h2 = frame_height
-    # crop_img_side = frame[0:frame_height, math.ceil(0.7*frame_width):math.ceil(0.90*frame_width)]
-    # crop_img_low = frame[math.ceil(0.6*frame_height):frame_height, math.ceil(0.3*frame_width):math.ceil(0.7*frame_width)]
-    # areas = [crop_img_side, crop_img_low]
-    # areas = [crop_img_side]
     areas = create_areas(areas_dict, frame)
+    
     # our output
     coords = {}
     transformed_coords = {}
@@ -190,41 +130,29 @@ def AnalyzeFrame(frame, computervision_client):
             coords[i] = item
             transformed_coords[i] = transform_coords(item, area)
             i = i + 1
-    # print("coords are:", coords)
-    #transformed_coords = {k: tuple((int(x[0]+0.7*frame_width), x[1]) for x in v) for k,v in coords.items()}
     print("fixed coords are:", transformed_coords)
+    #TODO: add argument to choose whether or not to send response (send and/or print)
     return
 
     b64img = base64.b64encode(cv2.imencode(".jpg", frame)[1])
-
     b64_encoded_frame = b64img.decode('utf-8')
-    # Check transformed coords:
-    # tmp_frame = frame
-    # for k, v in transformed_coords.items():
-    #     cv2.rectangle(tmp_frame, v[0], v[1], (255,0,0), 2)
-    # cv2.imshow("image", tmp_frame)
-    # cv2.waitKey(0)
     
+    #TODO: get this data from Shany's DB
     monitor_id = "90210"
-    #TODO: change monitor id
     json_string_fin = bounding_boxes_output_former(transformed_coords, monitor_id, b64_encoded_frame)
     # print(json_string_fin)
-    # print("--- %s seconds ---" % (time.time() - start_time))
     url = "http://rstreamapp.azurewebsites.net/api/UploadMonitorMapping"
-    headers={'Content-type':'application/json', 'Accept':'application/json'}
-    for trial in range(4):
+    headers = {'Content-type':'application/json', 'Accept':'application/json'}
+    for _ in range(4):
         while True:
             try:
                 response = requests.post(url, data=json_string_fin, headers=headers)
             except Exception as e:
                 print("Exception while posting:   |    ", e)
+                # TODO: throw exception if all trails ended unsuccefuly
                 continue
             break
-    #print(response)
-
-    #print('results for frame: ', result_list)
-
-     
+    #print(response)     
 
     # TODO: sanity check results (charecters etc.) and send them to somewhere
     return
